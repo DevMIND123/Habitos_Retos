c19402833f38600f463cb771d1fd5253822a9476
Luisa vargas <luchisvargasgomez@gmail.com>
2025-05-01 18:02:33 -0500

    Creación proyecto en springboot

---
+++ src/main/java/com/retochimba/habitos/habitos_retos/HabitosRetosApplication.java
@@ -0,0 +1,13 @@
+package com.retochimba.habitos.habitos_retos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitosRetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitosRetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/retochimba/habitos/habitos_retos/HabitosRetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.retochimba.habitos.habitos_retos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitosRetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
